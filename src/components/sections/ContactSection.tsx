7530d25490a6775ce21292fe29de6b5c38f160dc
juanan0309 <juan.quiroz@wizeline.com>
2021-12-28 08:04:18 -0500

    Add contact info

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -8,6 +8,7 @@ import {
   StyledContactSection,
 } from '../styles/sections/ContactSectionStyles'
 import { IoMail } from 'react-icons/io5'
+import { FaLinkedin } from 'react-icons/fa'
 
 import { SectionEnd } from '../styles/common/SectionEnd'
 
@@ -21,15 +22,20 @@ const ContactSection = () => {
       <ContactContainer>
         <ContactDescriptionContainer>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos fuga
-            aspernatur veniam! Illum possimus quos ducimus saepe consectetur
-            quisquam suscipit facere nobis soluta eum quas, ea tempore quae
-            incidunt modi!
+            Feel free to contact me through any of the links below. I am always
+            open to discussing new projects and opportunities.
           </p>
           <ContactLinksContainer>
             <ContactLink href="mailto:juanan0309@gmail.com" target="_blank">
               <IoMail />
               juanan0309@gmail.com
+            </ContactLink>
+            <ContactLink
+              href="https://www.linkedin.com/in/juan-andres-quiroz/"
+              target="_blank"
+            >
+              <FaLinkedin />
+              LinkedIn
             </ContactLink>
           </ContactLinksContainer>
         </ContactDescriptionContainer>
